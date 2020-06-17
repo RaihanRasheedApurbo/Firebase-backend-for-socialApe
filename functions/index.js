2157c28bbeb6d1edd3250f889c8e3708ee622e27
RaihanRasheedApurbo <coolest_apurbo@yahoo.com>
2020-06-17 13:20:48 +0600

    now we can add object in remote database

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,50 @@
 const functions = require('firebase-functions');
-
+const admin = require('firebase-admin');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
+
+admin.initializeApp(functions.config().firebase)
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello world of Firebase!");
 });
+
+exports.getScreams = functions.https.onRequest((request,response)=> {
+    admin.firestore().collection('screams').get()
+        .then(data => {
+            let screams = []
+            data.forEach(doc => {
+                screams.push(doc.data())
+            })
+            return response.json(screams)
+        })
+        .catch(err => console.error(err))
+
+
+})
+
+exports.createScream = functions.https.onRequest((request,response)=> {
+
+    if(request.method !== 'POST')
+    {
+        return response.status(400).json({error: 'You are not sending post request'})
+    }
+
+    const newScream = {
+        body: request.body.body,
+        userHandle: request.body.userHandle,
+        createdAt: admin.firestore.Timestamp.fromDate(new Date())
+    }
+
+    admin.firestore().collection('screams').add(newScream)
+        .then((doc) => {
+            response.json({message: `document ${doc} created successfully!`})
+        })
+        .catch((err) => {
+            response.status(500).json({ error: 'something went wrong'})
+            console.error(err)
+        })
+        
+
+})
