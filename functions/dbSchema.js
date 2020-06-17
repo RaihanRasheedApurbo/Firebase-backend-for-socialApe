42304e5fa40bae2cb555c117f4f875f70753cf9d
RaihanRasheedApurbo <coolest_apurbo@yahoo.com>
2020-06-17 20:51:41 +0600

    authrization part1 complete

---
+++ functions/dbSchema.js
@@ -0,0 +1,11 @@
+let db = {
+    screams: [
+        {
+            uderHandle: 'user',
+            body: 'this is the scream body',
+            createdAt: '2020-06-17T07:58:35.070Z',
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
--- functions/index.js
+++ functions/index.js
@@ -1,50 +1,123 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+admin.initializeApp()
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 
-admin.initializeApp(functions.config().firebase)
+const express = require('express')
+const app = express()
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello world of Firebase!");
-});
+//firebase authentication config
+var firebaseConfig = {
+    apiKey: "AIzaSyD-NjDRw0O5KH2Uf_zYRNSsGBNehE70GEw",
+    authDomain: "socialape-6fd1c.firebaseapp.com",
+    databaseURL: "https://socialape-6fd1c.firebaseio.com",
+    projectId: "socialape-6fd1c",
+    storageBucket: "socialape-6fd1c.appspot.com",
+    messagingSenderId: "437854909924",
+    appId: "1:437854909924:web:be0adc0442ffdd2d4343f1",
+    measurementId: "G-FNFLWQJLTR"
+  };
+const firebase = require('firebase')
+firebase.initializeApp(firebaseConfig)
+const db = admin.firestore()
 
-exports.getScreams = functions.https.onRequest((request,response)=> {
-    admin.firestore().collection('screams').get()
+app.get('/screams',(req,res) => {
+    db.collection('screams').orderBy('createdAt','desc').get()
         .then(data => {
             let screams = []
             data.forEach(doc => {
-                screams.push(doc.data())
+                screams.push({
+                    screamID: doc.id,
+                    ...doc.data()
+
+                })
             })
-            return response.json(screams)
+            return res.json(screams)
         })
         .catch(err => console.error(err))
-
-
 })
 
-exports.createScream = functions.https.onRequest((request,response)=> {
 
-    if(request.method !== 'POST')
-    {
-        return response.status(400).json({error: 'You are not sending post request'})
-    }
+// first demo code given when we initiated the project
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello world of Firebase!");
+// });
 
+
+
+// we have handle this case using combination of  app.get and exports.api
+// exports.getScreams = functions.https.onRequest((request,response)=> {
+//     admin.firestore().collection('screams').get()
+//         .then(data => {
+//             let screams = []
+//             data.forEach(doc => {
+//                 screams.push(doc.data())
+//             })
+//             return response.json(screams)
+//         })
+//         .catch(err => console.error(err))
+
+
+// })
+
+app.post('/scream',(request,response) => {
+    
     const newScream = {
         body: request.body.body,
         userHandle: request.body.userHandle,
-        createdAt: admin.firestore.Timestamp.fromDate(new Date())
+        createdAt: new Date().toISOString()
     }
 
-    admin.firestore().collection('screams').add(newScream)
-        .then((doc) => {
-            response.json({message: `document ${doc} created successfully!`})
+    db.collection('screams').add(newScream)
+        .then((data) => {
+            console.log(data)
+            return response.status(201).json({message: `document ${data.user.uid} created successfully!`})
         })
         .catch((err) => {
-            response.status(500).json({ error: 'something went wrong'})
             console.error(err)
+            return response.status(500).json({ error: 'something went wrong'})
+            
         })
         
 
 })
+ 
+ app.post('/signUp',(req,res) => {
+
+     const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        userHandle: req.body.userHandle
+     }
+     // we have to validate these inputs later....
+     db.doc(`/users/${newUser.handle}`).get()
+        .then(doc => {
+            if(doc.exists){
+                return res.status(400).json({handle: 'this handle is taken already!'})
+            }else{
+                return firebase.auth().createUserWithEmailAndPassword(newUser.email,newUser.password)
+                    
+                    
+
+            }
+        })
+        .then((data) => {
+            return data.user.getIdToken()
+                
+            
+        })
+        .then(token => {
+            return res.status(201).json({token})
+        })
+        .catch(err => {
+            //console.log('hi')
+            //console.error(err.code)
+            res.status(500).json({message: 'error message',err : err.code})
+        })
+     
+ })
+
+exports.api = functions.https.onRequest(app)
